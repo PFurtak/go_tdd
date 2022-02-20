acfeeabd6924af20899f3a1df237421902b5d022
Patrick Furtak <patrick@patrickfurtak.com>
2022-02-20 14:50:18 -0600

    Updates unit test for new requirements
    Greet should take in a string and greet the supplied
    name.
    
    Greet should greet the wold when supplied an empty
    string.

--- hello_world/greet_test.go
+++ hello_world/greet_test.go
@@ -3,11 +3,21 @@ package main
 import "testing"
 
 func TestGreet(t *testing.T) {
-	got := Greet()
-	want := "Hello World!"
+	t.Run("Greet with supplied name", func(t *testing.T) {
+		got := Greet("Patrick")
+		expected := "Hello, Patrick!"
 
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
-	}
+		if got != expected {
+			t.Errorf("got: %q, expected: %q", got, expected)
+		}
+	})
+	t.Run("Greet world when supplied empty string", func(t *testing.T) {
+		got := Greet("")
+		expected := "Hello, World!"
+
+		if got != expected {
+			t.Errorf("got: %q, expected: %q", got, expected)
+		}
+	})
 
 }
